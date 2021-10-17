f2da2f3d3324a9d004112bf256ae5d6ee2f2f63a
emy.yokoyama@burgerking.com.br <emy.yokoyama@burgerking.com.br>
2021-10-17 01:16:04 +0000

    Termino_Desafio_5_Materiazado_Tabela

--- models/northwind/mart/dim_shipper.sql
+++ models/northwind/mart/dim_shipper.sql
@@ -1,3 +1,5 @@
+{{ config( materialized='table') }}
+
 with selected as (
     select	
         id_transportador
--- models/northwind/mart/dim_supplier.sql
+++ models/northwind/mart/dim_supplier.sql
@@ -1,3 +1,5 @@
+{{ config( materialized='table') }}
+
 with selected as (
     select	
         supplier_id	
